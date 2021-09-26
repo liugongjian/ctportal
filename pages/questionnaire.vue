76963f37441292c67c680cf1e80fe146bc039dbd
Linghao Zhu <>
2021-09-26 15:42:16 +0800

    feat: submit button

--- pages/questionnaire.vue
+++ pages/questionnaire.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>天翼云视频监控需求问卷</div>
+  <div>
+    <div>天翼云视频监控需求问卷</div>
+    <el-button @click="submit">提交</el-button>
+  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  layout: 'h5'
+  layout: 'h5',
+  methods: {
+    submit() {
+      console.log('submit!')
+    }
+  }
 })
 </script>
