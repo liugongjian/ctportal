26105fcbe23433d6a8c2af3519b71d59274c5fca
liugongjian <liugongjianxin@163.com>
2022-01-04 10:20:05 +0800

    fix map

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,8 +13,8 @@ export default {
     script: [
       { src: 'https://res.wx.qq.com/open/js/jweixin-1.6.0.js' },
       // { type: 'text/javascript', src: 'https://webapi.amap.com/maps?v=1.4.15&key=f94d6b44b13dddb7d32c48302cd6f794&plugin=AMap.Scale,AMap.ToolBar,AMap.CustomLayer' },
-      { type: 'text/javascript', src: process.env.base  + 'echarts.min.js' },
-      { type: 'text/javascript', src: process.env.base + 'china.js' },
+      { type: 'text/javascript', src: (process.env.base || '/')  + 'echarts.min.js' },
+      { type: 'text/javascript', src: (process.env.base || '/') + 'china.js' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
