5a0d5436d7de5d283559011094ad148040aa40ca
liugongjian <liugongjianxin@163.com>
2022-01-06 09:20:25 +0800

    24

--- components/home/HomeMap.vue
+++ components/home/HomeMap.vue
@@ -21,7 +21,7 @@
         <li @mouseover="getRegionDetail('guizhou')" @click="fixRegion('guizhou')" :class="{'clicked':theRegion === 'guizhou'}">贵州云基地({{regions['guizhou'].length}})</li>
       </ul>
     </div>
-    <div class="test">{{(this.ele && this.ele.id) || '空'}}</div>
+    <div class="test">{{(this.ele && this.ele.id) || 'empty'}}</div>
   </div>
   </div>
 </template>
