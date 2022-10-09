42477ea68887244781f46182a98f493b031a495a
liugongjian <liugongjianxin@163.com>
2022-10-09 21:27:25 +0800

    'fix:箭头svg居中'

--- components/ListItem2.vue
+++ components/ListItem2.vue
@@ -3,8 +3,8 @@
     <div class="list__item2__img"><img :src="item.src" /></div>
     <div class="list__item2__word">
       <div class="list__item2__title">
-        {{item.title}}
-        <svg-icon height="18" width="14" :name="'vector'" color="red"/>
+        <span>{{item.title}}</span>
+        <svg-icon height="14" width="7" :name="'vector'" color="#fff #DF0629"/>
       </div>
       <div class="list__item2__content">{{item.content}}</div>
     </div>
@@ -40,6 +40,8 @@ export default class extends Vue{
     font-size: 18px;
     font-weight: bold;
     margin-bottom: 15px;
+    display:flex;
+    align-items:center;
     svg {
       margin-left: 12px;
     }
