3baf02ef366f7d71ef0816fbda871d9946f62e9d
Linghao Zhu <>
2022-10-09 13:43:47 +0800

    feat: 文案修改

--- components/service/ServiceSlider.vue
+++ components/service/ServiceSlider.vue
@@ -7,7 +7,7 @@ import { Vue, Component} from 'vue-property-decorator'
 @Component
 export default class extends Vue{
   private title = '支持与服务'
-  private desc = '服务工程师团队为您提供7*24小时技术服务支持。若您在使用百度智能云时遇到问题，您可以通过工单、智能客服、自助服务、热线电话等寻求帮助'
+  private desc = '服务工程师团队为您提供7*24小时技术服务支持。若您在使用天翼云视频监控产品时遇到问题，您可以通过工单系统寻求帮助'
   private fontStyle = { color: '#000' }
   // private picSrc = '~/assets/mock/home_slider.png'
   private picName = undefined
